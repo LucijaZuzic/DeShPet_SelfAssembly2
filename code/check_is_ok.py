0c92deccc06835bb02223c946f487c64c8527ce3
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-02-07 14:02:33 +0100

    Fix 20

--- code/check_is_ok.py
+++ code/check_is_ok.py
@@ -2,8 +2,140 @@
 from sklearn.model_selection import StratifiedKFold
 N_FOLDS_FIRST = 5
 import numpy as np
-from utils import DATA_PATH
+from utils import DATA_PATH, predictions_name, set_seed, AP_DATA_PATH, SP_DATA_PATH, AP_SP_DATA_PATH, TSNE_SP_DATA_PATH, TSNE_AP_SP_DATA_PATH
 import pandas as pd
+from sklearn.metrics import (
+    roc_curve,
+    precision_recall_curve,
+    roc_auc_score,
+    auc,
+    f1_score,
+)
+
+from custom_plots import (
+    my_accuracy_calculate,
+    weird_division,
+    convert_to_binary,
+)
+
+def returnGMEAN(actual, pred):
+    tn = 0
+    tp = 0
+    apo = 0
+    ane = 0
+    for i in range(len(pred)):
+        a = actual[i]
+        p = pred[i]
+        if a == 1:
+            apo += 1
+        else:
+            ane += 1
+        if p == a:
+            if a == 1:
+                tp += 1
+            else:
+                tn += 1
+
+    return np.sqrt(tp / apo * tn / ane)
+
+def read_ROC(test_labels, model_predictions, lines_dict, prthr, rocthr):
+    # Get false positive rate and true positive rate.
+    fpr, tpr, thresholds = roc_curve(test_labels, model_predictions)
+
+    # Calculate the g-mean for each threshold
+    gmeans = np.sqrt(tpr * (1 - fpr))
+
+    # Locate the index of the largest g-mean
+    ix = np.argmax(gmeans)
+
+    model_predictions_binary_thrROC = convert_to_binary(
+        model_predictions, rocthr
+    )
+
+    # Get recall and precision.
+    precision, recall, thresholdsPR = precision_recall_curve(
+        test_labels, model_predictions
+    )
+
+    # Calculate the F1 score for each threshold
+    fscore = []
+    for i in range(len(precision)):
+        fscore.append(
+            weird_division(2 * precision[i] * recall[i], precision[i] + recall[i])
+        )
+
+    # Locate the index of the largest F1 score
+    ixPR = np.argmax(fscore)
+
+    model_predictions_binary_thrPR = convert_to_binary(
+        model_predictions, prthr
+    )
+
+    model_predictions_binary = convert_to_binary(model_predictions, 0.5)
+
+    #lines_dict["ROC thr = "].append(rocthr)
+   # lines_dict["gmean (0.5) = "].append(
+    #    returnGMEAN(test_labels, model_predictions_binary)
+  #  )
+    lines_dict["gmean (PR thr) = "].append(
+        returnGMEAN(test_labels, model_predictions_binary_thrPR)
+    )
+  #  lines_dict["gmean (ROC thr) = "].append(
+  #      returnGMEAN(test_labels, model_predictions_binary_thrROC)
+  #  )
+  #  lines_dict["ROC AUC = "].append(roc_auc_score(test_labels, model_predictions))
+   # lines_dict["Accuracy (ROC thr) = "].append(
+   #     my_accuracy_calculate(test_labels, model_predictions, rocthr)
+   # ) 
+
+def read_PR(test_labels, model_predictions, lines_dict, prthr, rocthr):
+    # Get recall and precision.
+    precision, recall, thresholds = precision_recall_curve(
+        test_labels, model_predictions
+    )
+
+    # Calculate the F1 score for each threshold
+    fscore = []
+    for i in range(len(precision)):
+        fscore.append(
+            weird_division(2 * precision[i] * recall[i], precision[i] + recall[i])
+        )
+
+    # Locate the index of the largest F1 score
+    ix = np.argmax(fscore)
+    model_predictions_binary_thrPR = convert_to_binary(
+        model_predictions, prthr
+    )
+    model_predictions_binary = convert_to_binary(model_predictions, 0.5)
+
+    # Get false positive rate and true positive rate.
+    fpr, tpr, thresholdsROC = roc_curve(test_labels, model_predictions)
+
+    # Calculate the g-mean for each threshold
+    gmeans = np.sqrt(tpr * (1 - fpr))
+
+    # Locate the index of the largest g-mean
+    ixROC = np.argmax(gmeans)
+
+    model_predictions_binary_thrROC = convert_to_binary(
+        model_predictions, rocthr
+    )
+
+    #lines_dict["PR thr = "].append(prthr)
+    #lines_dict["PR AUC = "].append(auc(recall, precision))
+    #lines_dict["F1 (0.5) = "].append(f1_score(test_labels, model_predictions_binary))
+    lines_dict["F1 (PR thr) = "].append(
+        f1_score(test_labels, model_predictions_binary_thrPR)
+    )
+   # lines_dict["F1 (ROC thr) = "].append(
+   #     f1_score(test_labels, model_predictions_binary_thrROC)
+   # )
+    lines_dict["Accuracy (PR thr) = "].append(
+        my_accuracy_calculate(test_labels, model_predictions, prthr)
+    )
+    #lines_dict["Accuracy (0.5) = "].append(
+     #   my_accuracy_calculate(test_labels, model_predictions, 0.5)
+    #)
 
 # Algorithm settings
 N_FOLDS_FIRST = 5
@@ -48,7 +180,15 @@ new = [
 is_in_test = dict()
 
 for n in new:
-    is_in_test[n] = []
+    is_in_test[n] = dict()
+
+def read_one_prediction(some_path, test_number):
+    file = open(predictions_name(some_path, test_number), "r")
+    lines = file.readlines()
+    predictions = eval(lines[0])
+    labels = eval(lines[1])
+    file.close()
+    return predictions, labels
 
 for some_seed in seed_list: 
 
@@ -76,10 +216,81 @@ for some_seed in seed_list:
         for sq in new:
             if sq in test_seqs:
                 train_test_label.append("test")
-                is_in_test[sq].append((some_seed, test_number + 1))
+                is_in_test[sq][some_seed] = (test_number + 1, test_seqs.index(sq))
             if sq in train_seqs:
                 train_test_label.append("train")
         print(some_seed, test_number + 1, train_test_label.count("train"), train_test_label.count("test"), len(train_seqs), len(test_seqs), train_test_label.count("train") / len(train_seqs), train_test_label.count("test") / len(test_seqs))
+ 
+paths = [
+    AP_DATA_PATH,
+    SP_DATA_PATH,
+    AP_SP_DATA_PATH,
+    TSNE_SP_DATA_PATH,
+    TSNE_AP_SP_DATA_PATH,
+]
 
-print(is_in_test)
+predict_pep_path = dict()
+for pep in is_in_test:
+    predict_pep_path[pep] = dict()
+    for some_path in paths:
+        predict_pep_path[pep][some_path] = dict()
+        for some_seed in seed_list:
+            predict_pep_path[pep][some_path][some_seed] = dict()
+for pep in is_in_test:
+    for some_seed in seed_list:
+        test_number, ix = is_in_test[pep][some_seed]
+        set_seed(some_seed)
+        for some_path in paths:
+            all_predictions, all_labels = read_one_prediction(some_path, test_number) 
+            predict_pep_path[pep][some_path][some_seed] = (all_predictions[ix], all_labels[ix])
+ 
+vals_in_lines = [   
+    "gmean (PR thr) = ",
+    "F1 (PR thr) = ",
+    "Accuracy (PR thr) = ", 
+]
+       
+PRthr = {
+    AP_DATA_PATH: 0.2581173828,
+    SP_DATA_PATH: 0.31960518800000004,
+    AP_SP_DATA_PATH: 0.3839948796,
+    TSNE_SP_DATA_PATH: 0.30602415759999996,
+    TSNE_AP_SP_DATA_PATH: 0.34321978799999997,
+}
+ROCthr = {
+    AP_DATA_PATH: 0.5024869916000001,
+    SP_DATA_PATH: 0.5674178616000001,
+    AP_SP_DATA_PATH: 0.5708274524,
+    TSNE_SP_DATA_PATH: 0.566148754,
+    TSNE_AP_SP_DATA_PATH: 0.5588111376,
+}
+lines_dict_all = dict()
+for some_path in paths:  
+    lines_dict_all[some_path] = dict()  
+ 
+for some_path in paths: 
+    lines_dict = dict()
         
+    for val in vals_in_lines:
+        lines_dict[val] = []
+
+    for some_seed in seed_list:
+
+        preds = []
+        labs = []
+
+        for pep in new:
+            
+            pred, lab = predict_pep_path[pep][some_path][some_seed] 
+            preds.append(pred)
+            labs.append(lab)
+
+        read_PR(labs, preds, lines_dict, PRthr[some_path], ROCthr[some_path])
+        read_ROC(labs, preds, lines_dict, PRthr[some_path], ROCthr[some_path])
+ 
+
+    for val in lines_dict: 
+        lines_dict_all[some_path][val] = np.round(np.average(lines_dict[val]), 3)
+
+for p in lines_dict_all:  
+    print(p, lines_dict_all[p])
---
+++ code/hex_plot_2.py
@@ -0,0 +1,295 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+import sklearn
+import matplotlib.pyplot as plt
+from load_data import MAX_LEN
+from utils import (
+    predictions_hex_name,
+    scatter_name,
+    PATH_TO_NAME,
+    DATA_PATH,
+    AP_DATA_PATH,
+    SP_DATA_PATH,
+    AP_SP_DATA_PATH,
+    TSNE_SP_DATA_PATH,
+    TSNE_AP_SP_DATA_PATH,
+)
+from sklearn.metrics import (
+    roc_curve,
+    precision_recall_curve,
+    roc_auc_score,
+    auc,
+    f1_score,
+)
+from custom_plots import my_accuracy_calculate, weird_division, convert_to_binary
+
+plt.rcParams.update({"font.size": 22})
+PRthr = {
+    AP_DATA_PATH: 0.2581173828,
+    SP_DATA_PATH: 0.31960518800000004,
+    AP_SP_DATA_PATH: 0.3839948796,
+    TSNE_SP_DATA_PATH: 0.30602415759999996,
+    TSNE_AP_SP_DATA_PATH: 0.34321978799999997,
+}
+ROCthr = {
+    AP_DATA_PATH: 0.5024869916000001,
+    SP_DATA_PATH: 0.5674178616000001,
+    AP_SP_DATA_PATH: 0.5708274524,
+    TSNE_SP_DATA_PATH: 0.566148754,
+    TSNE_AP_SP_DATA_PATH: 0.5588111376,
+}
+
+
+def myfunc(x):
+    return slope * x + intercept
+
+
+def returnGMEAN(actual, pred):
+    tn = 0
+    tp = 0
+    apo = 0
+    ane = 0
+    for i in range(len(pred)):
+        a = actual[i]
+        p = pred[i]
+        if a == 1:
+            apo += 1
+        else:
+            ane += 1
+        if p == a:
+            if a == 1:
+                tp += 1
+            else:
+                tn += 1
+
+    return np.sqrt(tp / apo * tn / ane)
+
+
+def read_ROC(test_labels, model_predictions, lines_dict, oldPR, oldROC):
+    # Get false positive rate and true positive rate.
+    fpr, tpr, thresholds = roc_curve(test_labels, model_predictions)
+
+    # Calculate the g-mean for each threshold
+    gmeans = np.sqrt(tpr * (1 - fpr))
+
+    # Locate the index of the largest g-mean
+    ix = np.argmax(gmeans)
+
+    # Get recall and precision.
+    precision, recall, thresholdsPR = precision_recall_curve(
+        test_labels, model_predictions
+    )
+
+    # Calculate the F1 score for each threshold
+    fscore = []
+    for i in range(len(precision)):
+        fscore.append(
+            weird_division(2 * precision[i] * recall[i], precision[i] + recall[i])
+        )
+
+    # Locate the index of the largest F1 score
+    ixPR = np.argmax(fscore)
+
+    model_predictions_binary_thrPR_new = convert_to_binary(model_predictions, oldPR)
+    model_predictions_binary_thrROC_new = convert_to_binary(model_predictions, oldROC)
+
+    model_predictions_binary = convert_to_binary(model_predictions, 0.5)
+
+    lines_dict["ROC thr new = "].append(oldROC)
+    lines_dict["ROC AUC = "].append(roc_auc_score(test_labels, model_predictions))
+    lines_dict["gmean (0.5) = "].append(
+        returnGMEAN(test_labels, model_predictions_binary)
+    )
+    lines_dict["gmean (PR thr new) = "].append(
+        returnGMEAN(test_labels, model_predictions_binary_thrPR_new)
+    )
+    lines_dict["gmean (ROC thr new) = "].append(
+        returnGMEAN(test_labels, model_predictions_binary_thrROC_new)
+    )
+    lines_dict["Accuracy (ROC thr new) = "].append(
+        my_accuracy_calculate(test_labels, model_predictions, oldROC)
+    )
+
+
+def read_PR(test_labels, model_predictions, lines_dict, oldPR, oldROC):
+    # Get recall and precision.
+    precision, recall, thresholds = precision_recall_curve(
+        test_labels, model_predictions
+    )
+
+    # Calculate the F1 score for each threshold
+    fscore = []
+    for i in range(len(precision)):
+        fscore.append(
+            weird_division(2 * precision[i] * recall[i], precision[i] + recall[i])
+        )
+
+    # Locate the index of the largest F1 score
+    ix = np.argmax(fscore)
+
+    # Get false positive rate and true positive rate.
+    fpr, tpr, thresholdsROC = roc_curve(test_labels, model_predictions)
+
+    # Calculate the g-mean for each threshold
+    gmeans = np.sqrt(tpr * (1 - fpr))
+
+    # Locate the index of the largest g-mean
+    ixROC = np.argmax(gmeans)
+
+    model_predictions_binary_thrPR_new = convert_to_binary(model_predictions, oldPR)
+    model_predictions_binary_thrROC_new = convert_to_binary(model_predictions, oldROC)
+    model_predictions_binary = convert_to_binary(model_predictions, 0.5)
+
+    lines_dict["PR thr new = "].append(oldPR)
+    lines_dict["PR AUC = "].append(auc(recall, precision))
+    lines_dict["F1 (0.5) = "].append(f1_score(test_labels, model_predictions_binary))
+    lines_dict["F1 (PR thr new) = "].append(
+        f1_score(test_labels, model_predictions_binary_thrPR_new)
+    )
+    lines_dict["F1 (ROC thr new) = "].append(
+        f1_score(test_labels, model_predictions_binary_thrROC_new)
+    )
+    lines_dict["Accuracy (PR thr new) = "].append(
+        my_accuracy_calculate(test_labels, model_predictions_binary_thrPR_new, oldPR)
+    )
+    lines_dict["Accuracy (0.5) = "].append(
+        my_accuracy_calculate(test_labels, model_predictions, 0.5)
+    )
+
+
+offset = 1
+properties = np.ones(95)
+properties[0] = 0
+masking_value = 2
+df = pd.read_csv(
+    DATA_PATH + "41557_2022_1055_MOESM3_ESM_Figure3a_5mer_score_shortMD.csv"
+)
+
+
+new = [ 
+    "PTPCY",
+    "PPPHY",
+    "SYCGY",
+    "KWMDF",
+    "FFEKF",
+    "KWEFY",
+    "FKFEF",
+    "RWLDY",
+    "WKPYY",
+    "VVVVV",
+    "FKIDF",
+    "VKVFF",
+    "KFFFE",
+    "KFAFD",
+    "VKVEV",
+    "RVSVD", 
+    "KKFDD",
+    "VKVKV",
+    "KVKVK",
+    "DPDPD",
+]
+
+dict_hex = {}
+ix_to_skip = []
+peps_skipped = []
+ix = 0
+for i in df["pep"]:
+    find_err = False
+    for n in new:
+        if i.lower() in n.lower() or n.lower() in i.lower():
+            find_err = True
+    if find_err:
+        ix_to_skip.append(ix)
+        peps_skipped.append(i) 
+    ix += 1
+    dict_hex[i] = "1"
+print(len(df["pep"]), len(df["pep"]) - len(ix_to_skip))
+
+actual_AP = []
+for i in df["AP"]:
+    actual_AP.append(i)
+
+seq_example = ""
+for i in range(MAX_LEN):
+    seq_example += "A"
+dict_hex[seq_example] = "1"
+
+test_labels = []
+threshold = 1.75
+for i in df["AP"]:
+    if i < threshold:
+        test_labels.append(0)
+    else:
+        test_labels.append(1)
+
+path_list = [
+    AP_DATA_PATH,
+    SP_DATA_PATH,
+    AP_SP_DATA_PATH,
+    TSNE_SP_DATA_PATH,
+    TSNE_AP_SP_DATA_PATH,
+]
+
+vals_in_lines = [
+    "ROC AUC = ",
+    "PR AUC = ",
+    "gmean (0.5) = ",
+    "F1 (0.5) = ",
+    "Accuracy (0.5) = ",
+    "ROC thr new = ",
+    "PR thr new = ",
+    "gmean (ROC thr new) = ",
+    "F1 (ROC thr new) = ",
+    "Accuracy (ROC thr new) = ",
+    "gmean (PR thr new) = ",
+    "F1 (PR thr new) = ",
+    "Accuracy (PR thr new) = ",
+]
+
+lines_dict = dict()
+for val in vals_in_lines:
+    lines_dict[val] = []
+
+print(ix_to_skip, peps_skipped)
+
+for some_path in path_list:
+    fileopen = open(predictions_hex_name(some_path), "r")
+    predictions = eval(fileopen.readlines()[0].replace("\n", ""))
+    predictions = predictions[:-1]
+    fileopen.close()
+
+    predictions_NEW = []
+    test_labels_NEW = []
+    actual_AP_NEW = []
+    for ix in range(len(predictions)):
+        if ix not in ix_to_skip:
+            predictions_NEW.append(predictions[ix])
+            test_labels_NEW.append(test_labels[ix])
+            actual_AP_NEW.append(actual_AP[ix])
+
+    slope, intercept, r, p, std_err = stats.linregress(predictions_NEW, actual_AP_NEW)
+    print(some_path)
+    print("R: " + str(r))
+    print("corrcoef: " + str(np.round(np.corrcoef(predictions_NEW, actual_AP_NEW)[0][1], 2)))
+    print("spearmanr: " + str(np.round(stats.spearmanr(predictions_NEW, actual_AP_NEW)[0], 2)))
+    print("R2: " + str(np.round(sklearn.metrics.r2_score(predictions_NEW, actual_AP_NEW), 2)))
+
+    read_ROC(test_labels_NEW, predictions_NEW, lines_dict, PRthr[some_path], ROCthr[some_path])
+    read_PR(test_labels_NEW, predictions_NEW, lines_dict, PRthr[some_path], ROCthr[some_path])
+
+    mymodel = list(map(myfunc, predictions_NEW))
+    plt.title(PATH_TO_NAME[some_path] + " model")
+    plt.xlabel("Predicted self assembly probability")
+    plt.ylabel("AP")
+    plt.plot(predictions_NEW, mymodel, color="#ff120a")
+    plt.scatter(predictions_NEW, actual_AP_NEW, color="#2e85ff")
+    plt.savefig(scatter_name(some_path), bbox_inches="tight")
+    plt.close()
+
+for x in lines_dict:
+    print(x)
+    if "Acc" in x:
+        print(np.round(lines_dict[x], 1))
+    else:
+        print(np.round(lines_dict[x], 3))

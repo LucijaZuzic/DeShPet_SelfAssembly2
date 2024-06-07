b3c3f289eb6e392042abbd23a21e656cd0ff1c42
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-06-07 08:51:14 +0200

    Avg std

--- code/print_preds_and_vals.py
+++ code/print_preds_and_vals.py
@@ -50,17 +50,25 @@ def print_latex_format(model_list, path_to_file):
     else:
         metric_ord = [2, 7, 8, 9, 3, 10, 11, 12, 4, 5, 6, 0, 1]
 
+    new_csv = {"Metric": []}
+    for ix in metric_ord:
+        new_csv["Metric"].append(pd_file["Metric"][ix] + " (avg.)")
+        new_csv["Metric"].append(pd_file["Metric"][ix] + " (std.)")
     for ix in metric_ord:
         rows_string_one = pd_file["Metric"][ix]
         max_row = -1
         max_part = "be"
         for model in model_list:
+            if model not in new_csv:
+                new_csv[model] = []
             vals_use = []
             for seed in seed_list:
                 vals_use.append(pd_file[model + " (seed " + str(seed) + ")"][ix])
             round_val = 3
             if "Acc" in pd_file["Metric"][ix]:
                 round_val = 1
+            new_csv[model].append(np.average(vals_use))
+            new_csv[model].append(np.std(vals_use))
             part_avg = str(np.round(np.average(vals_use), round_val))
             part_std = str(np.round(np.std(vals_use), round_val))
             if "Acc" in pd_file["Metric"][ix]:
@@ -73,7 +81,9 @@ def print_latex_format(model_list, path_to_file):
                 max_part = part
         rows_string_one += " \\\\ \\hline\n"
         rows_string += rows_string_one.replace(max_part, "\\textbf{" + max_part + "}")
+    new_df = pd.DataFrame(new_csv)
+    new_df.to_csv(path_to_file.replace(".csv", "_avg_std.csv"))
     print(rows_string)
 
 print_latex_format(model_list_new, "review/long/3_24.csv")
-print_latex_format(model_list_new, "review_20/long/3_24.csv")
+print_latex_format(model_list_new, "review_20/long/5_5.csv")
